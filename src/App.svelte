f2c7547e546f9a570d1091836a3d4bc49e608911
William Lachance <wlachance@mozilla.com>
2020-08-24 12:28:34 -0400

    Template + license

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+  import Routes from "./routes/Routes.svelte";
+
+  export let title;
+</script>
+
+<h1>{title}</h1>
+
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+</nav>
+
+<Routes />
