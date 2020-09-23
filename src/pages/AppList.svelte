6feaca73d7ab02be70ae458024ca5d988d23482d
William Lachance <wrlach@gmail.com>
2020-09-23 15:38:14 -0400

    Project list

---
+++ src/pages/AppList.svelte
@@ -0,0 +1,23 @@
+<script>
+  const URL = "data/apps.json";
+  let apps = undefined;
+  const request = fetch(URL)
+    .then(r => r.json())
+    .then(ret => {
+      apps = ret.sort((a, b) => a.app_id > b.app_id);
+      console.log(apps);
+    });
+</script>
+
+<h2>Applications</h2>
+{#if apps}
+  {#each apps as app}
+    <p>
+      <a href="#/{app.app_id}">{app.name}</a>
+      {#if app.description}
+        &nbsp;
+        <i>{app.description}</i>
+      {/if}
+    </p>
+  {/each}
+{/if}
