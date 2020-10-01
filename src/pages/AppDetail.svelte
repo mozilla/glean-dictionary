4a97aef91f85d9b0ae89bfc572ff31baeca01bfc
William Lachance <wrlach@gmail.com>
2020-10-01 17:12:07 -0400

    Add metric pages

--- src/pages/AppDetail.svelte
+++ src/pages/AppDetail.svelte
@@ -13,12 +13,21 @@
 
 {#if app}
   <p>{app.description}</p>
-  <h3>Pings</h3>
+  <h2>Pings</h2>
   <ul>
     {#each app.pings as ping}
       <li>
         <a href={`/apps/${params.app}/pings/${ping.name}`}>{ping.name}</a>
         <i>{ping.description}</i>
+      </li>
+    {/each}
+  </ul>
+  <h2>Metrics</h2>
+  <ul>
+    {#each app.metrics as metric}
+      <li>
+        <a href={`/apps/${params.app}/metrics/${metric.name}`}>{metric.name}</a>
+        <i>{metric.description}</i>
       </li>
     {/each}
   </ul>
