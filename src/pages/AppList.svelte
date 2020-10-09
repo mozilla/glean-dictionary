f75587439024e2108e31b3ec9f0259cc3a339df8
Mahfuza Humayra Mohona <14244685+mhmohona@users.noreply.github.com>
2020-10-09 22:39:07 +0600

    Update AppList.svelte

--- src/pages/AppList.svelte
+++ src/pages/AppList.svelte
@@ -14,15 +14,34 @@
   function filterApps(filterText) {
     filteredApps = apps.filter((appItem) => appItem.name.includes(filterText));
   }
+  let showDeprecated = true;
 </script>
 
 <h2>Applications</h2>
-<FilterInput onChangeText={filterApps} />
-{#if apps}
+
+<label>
+  <input type="checkbox" bind:checked={showDeprecated} />
+  Show deprecated applications
+</label>
+
+{#if showDeprecated}
+  <FilterInput onChangeText={filterApps} />
+  {#if apps}
+    {#each filteredApps as app}
+      <p>
+        <a href="/apps/{app.name}">{app.name}</a>
+        {#if app.description}<i>{app.description}</i>{/if}
+      </p>
+    {/each}
+  {/if}
+{:else if apps}
+  <FilterInput onChangeText={filterApps} />
   {#each filteredApps as app}
-    <p>
-      <a href="/apps/{app.name}">{app.name}</a>
-      {#if app.description}<i>{app.description}</i>{/if}
-    </p>
+    {#if !app.deprecated}
+      <p>
+        <a href="/apps/{app.name}">{app.name}</a>
+        {#if app.description}<i>{app.description}</i>{/if}
+      </p>
+    {/if}
   {/each}
 {/if}
