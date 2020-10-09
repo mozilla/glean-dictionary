1a0716504b68a8ac46f7eb6198c01e07f759f6cd
Mahfuza Humayra Mohona <14244685+mhmohona@users.noreply.github.com>
2020-10-09 20:15:49 +0600

    Updated AppList according to review

--- src/pages/AppList.svelte
+++ src/pages/AppList.svelte
@@ -1,7 +1,6 @@
 <script>
-	import { fade } from 'svelte/transition';
-	let visible = true;
-
+  import { fade } from "svelte/transition";
+  let showDeprecated = true;
   const URL = "data/apps.json";
   let apps;
   fetch(URL)
@@ -14,32 +13,26 @@
 <h2>Applications</h2>
 
 <label>
-	<input type="checkbox" bind:checked={visible}>
-	Show deprecated applications
+  <input type="checkbox" bind:checked={showDeprecated} />
+  Show deprecated applications
 </label>
 
-{#if visible}
-	<p transition:fade>
-		{#if apps}
-     {#each apps as app}
-       <p>
-       <a href="/apps/{app.name}">{app.name}</a>
-       {#if app.description}<i>{app.description}</i>{/if}
-       </p>
-      {/each}
-    {/if}   
-	</p>
-
-{:else}
+{#if showDeprecated}
   {#if apps}
-  {#each apps as app}    
-    {#if !app.deprecated}
-      <p> 
+    {#each apps as app}
+      <p>
         <a href="/apps/{app.name}">{app.name}</a>
         {#if app.description}<i>{app.description}</i>{/if}
-     </p>
+      </p>
+    {/each}
   {/if}
+{:else if apps}
+  {#each apps as app}
+    {#if !app.deprecated}
+      <p>
+        <a href="/apps/{app.name}">{app.name}</a>
+        {#if app.description}<i>{app.description}</i>{/if}
+      </p>
+    {/if}
   {/each}
-{/if}
-   
 {/if}
