20290182d0a938b44ddc337d7b7b837e5bf6c63d
thedatumgirl <59704142+TheDatumGirl@users.noreply.github.com>
2020-10-11 23:07:01 +0530

    Wlach's suggestions added
    Removed divs and added a simple table instead of description list

--- src/pages/MetricDetail.svelte
+++ src/pages/MetricDetail.svelte
@@ -6,112 +6,122 @@
   const metricDataPromise = getMetricData(params.app, params.metric);
 </script>
 
+<style>
+  .table-header {
+    @apply table-auto;
+    @apply my-4;
+  }
+  .table-header td {
+    @apply border;
+    @apply p-2;
+  }
+</style>
+
 {#await metricDataPromise then metric}
   <h1>{metric.name}</h1>
-  <h2>{metric.description}</h2>
-  <div />
-  <section id="metric-detail-view">
-    <div>
-      <footer>
-        <div>
-          <div>
-            <dl>
-              <dt>
-                relevant bugs :
-                {#each metric.bugs as bug, i}
-                  {#if bug.indexOf('http') > -1}
-                    <a href={bug} title={bug} target="_blank"> {i + 1} </a>
-                  {:else}<span>{bug}</span>{/if}
-                {/each}
-              </dt>
-            </dl>
-            <dl>
-              <dt>send in pings : {metric.send_in_pings.join(', ')}</dt>
-            </dl>
-            {#if metric.lifetime}
-              <dl>
-                <dt>
-                  <a
-                    href="https://mozilla.github.io/glean/book/user/adding-new-metrics.html?highlight=lifetime#when-should-glean-automatically-clear-the-measurement"
-                    target="_blank">
-                    lifetime
-                  </a>
-                  {metric.lifetime}
-                </dt>
-              </dl>
-            {/if}
-            {#if metric.time_unit}
-              <dl>
-                <dt>
-                  <a
-                    href="https://mozilla.github.io/glean/book/user/metrics/timing_distribution.html"
-                    target="_blank">
-                    time unit
-                  </a>
-                  {metric.time_unit}
-                </dt>
-              </dl>
-            {/if}
-            {#if metric.bucket_count}
-              <dl>
-                <dt>bucket count : {metric.bucket_count}</dt>
-              </dl>
-            {/if}
-            {#if metric.histogram_type}
-              <dl>
-                <dt>histogram type : {metric.histogram_type}</dt>
-              </dl>
-            {/if}
-            {#if metric.unit}
-              <dl>
-                <dt>unit : {metric.unit}</dt>
-              </dl>
-            {/if}
-            {#if metric.range_min !== undefined}
-              <dl>
-                <dt>range minimum</dt>
-                <dd>{metric.range_min}</dd>
-              </dl>
-            {/if}
-            {#if metric.range_max !== undefined}
-              <dl>
-                <dt>range maximum</dt>
-                <dd>{metric.range_max}</dd>
-              </dl>
-            {/if}
-            <dl>
-              <dt>disabled : {metric.disabled}</dt>
-            </dl>
-            {#if metric.data_reviews && metric.data_reviews.length}
-              <dl>
-                <dt>
-                  data reviews
-                  {#each metric.data_reviews as rev, i}
-                    {#if rev.indexOf('http') > -1}
-                      <a href={rev} title={rev} target="_blank"> {i + 1} </a>
-                    {:else}<span>{rev}</span>{/if}
-                  {/each}
-                </dt>
-              </dl>
-            {/if}
-            {#if metric.no_lint && metric.no_lint.length}
-              <dl>
-                <dt>no_lint : {metric.no_lint.join(', ')}</dt>
-              </dl>
-            {/if}
-            {#if metric.labels && metric.labels.length}
-              <dl>
-                <dt>labels : {metric.labels.join(', ')}</dt>
-              </dl>
-            {/if}
-            {#if metric.version || metric.version === 0}
-              <dl>
-                <dt>version : {metric.version}</dt>
-              </dl>
-            {/if}
-          </div>
-        </div>
-      </footer>
-    </div>
-  </section>
+  <p>{metric.description}</p>
+  <table class="table-header">
+    <tr>
+      <td>Relevant Bugs</td>
+      <td>
+        {#each metric.bugs as bug, i}
+          {#if bug.indexOf('http') > -1}
+            <a href={bug} title={bug} target="_blank"> {i + 1} </a>
+          {:else}<span>{bug}</span>{/if}
+        {/each}
+      </td>
+    </tr>
+    <tr>
+      <td>Send In Pings</td>
+      <td>{metric.send_in_pings.join(', ')}</td>
+    </tr>
+    {#if metric.lifetime}
+      <tr>
+        <td>
+          <a
+            href="https://mozilla.github.io/glean/book/user/adding-new-metrics.html?highlight=lifetime#when-should-glean-automatically-clear-the-measurement"
+            target="_blank">
+            Lifetime
+          </a>
+        </td>
+        <td>{metric.lifetime}</td>
+      </tr>
+    {/if}
+    {#if metric.time_unit}
+      <tr>
+        <td>
+          <a
+            href="https://mozilla.github.io/glean/book/user/metrics/timing_distribution.html"
+            target="_blank">
+            Time unit
+          </a>
+        </td>
+        <td>{metric.time_unit}</td>
+      </tr>
+    {/if}
+    {#if metric.bucket_count}
+      <tr>
+        <td>Bucket count</td>
+        <td>{metric.bucket_count}</td>
+      </tr>
+    {/if}
+    {#if metric.histogram_type}
+      <tr>
+        <td>Histogram type</td>
+        <td>{metric.histogram_type}</td>
+      </tr>
+    {/if}
+    {#if metric.unit}
+      <tr>
+        <td>Unit</td>
+        <td>{metric.unit}</td>
+      </tr>
+    {/if}
+    {#if metric.range_min !== undefined}
+      <tr>
+        <td>Range Minimum</td>
+        <td>{metric.range_min}</td>
+      </tr>
+    {/if}
+    {#if metric.range_max !== undefined}
+      <tr>
+        <td>Range Maximum</td>
+        <td>{metric.range_max}</td>
+      </tr>
+    {/if}
+    <tr>
+      <td>Disabled</td>
+      <td>{metric.disabled}</td>
+    </tr>
+    {#if metric.data_reviews && metric.data_reviews.length}
+      <tr>
+        <td>Data Reviews</td>
+        <td>
+          {#each metric.data_reviews as rev, i}
+            {#if rev.indexOf('http') > -1}
+              <a href={rev} title={rev} target="_blank"> {i + 1} </a>
+            {:else}<span>{rev}</span>{/if}
+          {/each}
+        </td>
+      </tr>
+    {/if}
+    {#if metric.no_lint && metric.no_lint.length}
+      <tr>
+        <td>No_lint</td>
+        <td>{metric.no_lint.join(', ')}</td>
+      </tr>
+    {/if}
+    {#if metric.labels && metric.labels.length}
+      <tr>
+        <td>Labels</td>
+        <td>{metric.labels.join(', ')}</td>
+      </tr>
+    {/if}
+    {#if metric.version || metric.version === 0}
+      <tr>
+        <td>Version</td>
+        <td>{metric.version}</td>
+      </tr>
+    {/if}
+  </table>
 {/await}
