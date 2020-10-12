9232af245dceedd0aadb0c6bfe08af38c147a395
DawoodShahat <dawoodshahat@gmail.com>
2020-10-13 03:37:18 +0500

    update sorting

--- src/pages/AppList.svelte
+++ src/pages/AppList.svelte
@@ -9,7 +9,7 @@
 
   onMount(async () => {
     apps = await fetchJSON(URL);
-    apps.sort((a, b) => a.app_id > b.app_id);
+    apps.sort((a, b) => a.app_id > b.app_id ? 1 : -1);
     filteredApps = apps;
   });
 
