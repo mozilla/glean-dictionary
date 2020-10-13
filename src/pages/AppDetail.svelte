c7b2b2cc6ebd82d991b9430590ab11ac0ee844a8
DawoodShahat <dawoodshahat@gmail.com>
2020-10-13 22:59:08 +0500

    add FilterInput metrics

--- src/pages/AppDetail.svelte
+++ src/pages/AppDetail.svelte
@@ -1,14 +1,24 @@
 <script>
   import { onMount } from "svelte";
   import { fetchJSON } from "../state/api";
+  import FilterInput from "../components/FilterInput.svelte";
 
   export let params;
   const URL = `data/${params.app}/index.json`;
   let app;
+  let filteredMetrics;
 
   onMount(async () => {
     app = await fetchJSON(URL);
+    filteredMetrics = app.metrics;
   });
+
+  function filterMetrics(filterText) {
+    filteredMetrics = app.metrics.filter((metric) =>
+      metric.name.includes(filterText)
+    );
+  }
+
 </script>
 
 <h1>{params.app}</h1>
@@ -24,8 +34,9 @@
     {/each}
   </ul>
   <h2>Metrics</h2>
+  <FilterInput onChangeText={filterMetrics} />
   <ul>
-    {#each app.metrics as metric}
+    {#each filteredMetrics as metric}
       <li>
         <a href={`/apps/${params.app}/metrics/${metric.name}`}>{metric.name}</a>
         <i>{metric.description}</i>
