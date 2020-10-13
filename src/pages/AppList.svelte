e74eea8ed642f767710cb093c8422b39db709b76
Dawood Shahat <dawoodshahat@gmail.com>
2020-10-13 18:19:35 +0500

    Update app sorting (#104)
    Co-authored-by: William Lachance <wrlach@gmail.com>

--- src/pages/AppList.svelte
+++ src/pages/AppList.svelte
@@ -9,7 +9,7 @@
 
   onMount(async () => {
     apps = await fetchJSON(URL);
-    apps.sort((a, b) => a.app_id > b.app_id);
+    apps.sort((a, b) => (a.name > b.name ? 1 : -1));
     filteredApps = apps;
   });
 
