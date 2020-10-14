c1d840d399474ac65c4e6ed863660ccf57c8cca2
jailang <lynjailang@gmail.com>
2020-10-14 03:17:32 -0700

    Update storybook webpack config to use Tailwind

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,13 @@
+const sveltePreprocess = require("svelte-preprocess");
 module.exports = {
   stories: ["../stories/**/*.stories.js"],
   addons: ["@storybook/addon-knobs"],
+  webpackFinal: async (config) => {
+    const svelteLoader = config.module.rules.find(r => r.loader && r.loader.includes("svelte-loader"));
+    svelteLoader.options = {
+      ...svelteLoader.options,
+      preprocess: sveltePreprocess({ postcss: true })
+    };
+    return config;  
+  }
 };
