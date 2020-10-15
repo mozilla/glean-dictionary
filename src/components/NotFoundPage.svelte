72b962dbaed5392f669e7567aa28b3efeecbdb72
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2020-10-14 21:55:37 -0400

    Created initial Not Found page component

---
+++ src/components/NotFoundPage.svelte
@@ -0,0 +1,20 @@
+<script>
+const app = new App({
+    target: document.body,
+    props: {
+        application: application,
+    }
+})
+
+</script>
+
+<style>
+    h2 {
+        color: lightgray;
+        font: italic;
+    }
+
+</style>
+
+<h1>Sorry!</h1>
+<h2>Could not find that page.</h2>
