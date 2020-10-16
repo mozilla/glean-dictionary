d51922923c537ffb8932c30ac46de6452d927012
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2020-10-15 21:49:07 -0400

    Imported other component data

--- src/components/NotFoundPage.svelte
+++ src/components/NotFoundPage.svelte
@@ -1,8 +1,18 @@
 <script>
-const app = new App({
+    import AppDetail from "../pages/AppDetail.svelte";
+    import AppList from "../pages/AppList.svelte";
+    import PingDetail from "../pages/PingDetail.svelte";
+    import MetricDetail from "../pages/MetricDetail.svelte";
+    import TableDetail from "../pages/TableDetail.svelte";
+
+ let app = new App({
     target: document.body,
     props: {
-        application: application,
+        app: AppDetail,
+        list: AppList,
+        ping: PingDetail,
+        metric: MetricDetail,
+        table: TableDetail,
     }
 })
 
@@ -17,4 +27,4 @@ const app = new App({
 </style>
 
 <h1>Sorry!</h1>
-<h2>Could not find that page.</h2>
+<h2>Could not find {app.props} {app.props.name}.</h2>
