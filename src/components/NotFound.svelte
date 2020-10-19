e7df3c00992513e77b78780ad1815ccc9e4b0332
Rhys <52160097+nonbinaryfrog@users.noreply.github.com>
2020-10-19 16:42:52 -0400

    "Updated not found name"

---
+++ src/components/NotFound.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let pageName;
+  export let itemType;
+</script>
+
+<style>
+  h2 {
+    color: darkgray;
+    font-style: italic;
+  }
+</style>
+
+<h1>Sorry!</h1>
+<h2>Could not find {itemType} {pageName}.</h2>
