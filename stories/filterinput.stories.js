30ef6e56d7dee0c4b6d774d0dc6c4a94639f5630
DawoodShahat <dawoodshahat@gmail.com>
2020-10-22 12:34:48 +0500

    added_suggested_changes

--- stories/filterinput.stories.js
+++ stories/filterinput.stories.js
@@ -1,10 +1,16 @@
-import FilterInput from "../src/components/FilterInput.svelte";
+import FilterableList from "./FilterableList.svelte";
+import { withKnobs, array } from "@storybook/addon-knobs";
+
+const listItems = ["foo", "bar", "foobar", "lorem", "ipsum"];
 
 export default {
   title: "FilterInput",
+  decorators: [withKnobs],
 };
 
 export const Basic = () => ({
-  Component: FilterInput,
-  props: "",
+  Component: FilterableList,
+  props: {
+    listItems: array("listItems", listItems),
+  },
 });
--- stories/pill.stories.js
+++ stories/pill.stories.js
@@ -1,13 +1,15 @@
 import Pill from "../src/components/Pill.svelte";
+import { withKnobs, text, color } from "@storybook/addon-knobs";
 
 export default {
   title: "Pill",
+  decorators: [withKnobs],
 };
 
 export const Deprecated = () => ({
   Component: Pill,
   props: {
-    message: "Deprecated",
-    bgColor: "#4a5568",
+    message: text("message", "Deprecated"),
+    bgColor: color("bgColor", "#4a5568"),
   },
 });
