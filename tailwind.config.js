f5bec1d6a43b9504d43c6950c6d5b3af08c386da
jailang <lynjailang@gmail.com>
2020-10-22 12:13:20 -0700

    Optimize default extractor

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,5 @@
 const production = !process.env.ROLLUP_WATCH;
 
-const svelteExtractor = (content) => {
-  const regExp = new RegExp(/[A-Za-z0-9-_:/]+/g);
-  const matchedTokens = [];
-  let match = regExp.exec(content);
-
-  while (match) {
-    if (match[0].startsWith("class")) {
-      matchedTokens.push(match[0].substring(6));
-    } else {
-      matchedTokens.push(match[0]);
-    }
-    match = regExp.exec(content);
-  }
-  return matchedTokens;
-};
-
 module.exports = {
   future: {},
   purge: {
@@ -23,7 +7,11 @@ module.exports = {
     enabled: production, // disable purge in dev
     options: {
       whitelist: [/svelte-/],
-      defaultExtractor: svelteExtractor,
+      /* eslint-disable no-unused-vars */
+      defaultExtractor: (content) =>
+        [...content.matchAll(/(?:class:)*([\w\d-/:%.]+)/gm)].map(
+          ([_match, group, ..._rest]) => group
+        ),
     },
   },
   theme: {
