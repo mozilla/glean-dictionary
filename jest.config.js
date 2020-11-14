bfa44bee14efe1d0ad0e5576ecbec4b6b3b139b3
DawoodShahat <dawoodshahat@gmail.com>
2020-11-15 04:34:42 +0500

    snapshot_tests

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  transform: {
+    "^.+\\.svelte$": "svelte-jester",
+    "^.+\\.js$": "babel-jest",
+  },
+  moduleFileExtensions: ["js", "svelte", "json"],
+  testPathIgnorePatterns: ["/node_modules/", "/public/", "/storybook-static/"],
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/__mocks__/styleMock.js",
+  },
+};
