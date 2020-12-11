f2ea07f6e30092c84b21f8e89e66bd927b14d4e0
William Lachance <wrlach@gmail.com>
2020-12-11 12:44:33 -0500

    Fix replacement function

--- src/state/urls.js
+++ src/state/urls.js
@@ -2,5 +2,5 @@ export function getMetricURL(appName, metricName) {
   // workaround servers that incorrectly interpret url resources with a "." in
   // them as having an extension (this is common for metrics) -- the glean
   // schema doesn't allow `-`'s in metrics, so this should be ok
-  return `/apps/${appName}/metrics/${metricName.replace(/-/g, "i")}`;
+  return `/apps/${appName}/metrics/${metricName.replace(/\./g, "-")}`;
 }
--- tests/state.urls.test.js
+++ tests/state.urls.test.js
@@ -2,6 +2,6 @@ import { getMetricURL } from "../src/state/urls";
 
 describe("metric URL replacing", () => {
   it("works as expected", () => {
-    expect(getMetricURL("foo", "bar.baz")).toEqual("/apps/foo/metrics/bar.baz");
+    expect(getMetricURL("foo", "bar.baz")).toEqual("/apps/foo/metrics/bar-baz");
   });
 });
