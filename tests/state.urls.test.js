74675a574a5abd199022dacef3a33be2d43417bb
William Lachance <wlachance@mozilla.com>
2020-12-16 15:52:03 -0500

    (Re)Fix URL escaping (#281)
    This restores the URL escaping behaviour in #260 which was accidentally
    reverted in #276.

--- tests/state.urls.test.js
+++ tests/state.urls.test.js
@@ -1,7 +1,9 @@
-import { getMetricURL } from "../src/state/urls";
+import { getItemURL } from "../src/state/urls";
 
 describe("metric URL replacing", () => {
   it("works as expected", () => {
-    expect(getMetricURL("foo", "bar.baz")).toEqual("/apps/foo/metrics/bar-baz");
+    expect(getItemURL("foo", "metrics", "bar.baz")).toEqual(
+      "/apps/foo/metrics/bar-baz"
+    );
   });
 });
