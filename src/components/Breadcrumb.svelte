7275f24b96651a7afe315f58841dc63ad593bc13
William Lachance <wrlach@gmail.com>
2020-12-22 11:56:27 -0500

    WIP

--- src/components/Breadcrumb.svelte
+++ src/components/Breadcrumb.svelte
@@ -2,7 +2,7 @@
   export let links;
 </script>
 
-<ol class="flex border-b-2 py-2 divide-x {!links.length ? 'hidden' : ''}">
+<ol class="flex py-2 divide-x {!links.length ? 'hidden' : ''}">
   {#each links as link}
     <li class="px-2"><a href={link.url}>{link.name}</a></li>
   {/each}
