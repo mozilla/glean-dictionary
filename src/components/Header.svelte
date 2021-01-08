4611172985d911a05956ae18ee633e18720f46b0
William Lachance <wrlach@gmail.com>
2021-01-08 16:22:29 -0500

    More searchbox wip

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,5 +1,6 @@
 <script>
   import Breadcrumb from "./Breadcrumb.svelte";
+  import SearchBox from "./SearchBox.svelte";
 
   export let links = [];
 </script>
@@ -15,43 +16,6 @@
       inset 0 10px 4px -10px rgba(9, 32, 77, 0.12),
       inset 0 10px 3px -10px rgba(29, 17, 51, 0.12);
   }
-
-  .search-autocomplete {
-    margin-left: 24px;
-    margin-top: 24px;
-    min-width: 480px;
-    position: relative;
-  }
-
-  .search-input {
-    margin: 0;
-    width: 100%;
-  }
-
-  .search-results {
-    position: absolute;
-    border: 1px solid #dbdbdb;
-    background-color: white;
-    width: 100%;
-    z-index: 100;
-  }
-  .search-results ul {
-    margin: 0;
-    padding: 0;
-  }
-
-  .search-results li {
-    padding: 0.25rem 0.5rem;
-    cursor: pointer;
-  }
-
-  .search-results li {
-    padding: 0.25rem 0.5rem;
-    cursor: pointer;
-  }
-  .search-results li:hover {
-    background-color: #dbdbdb;
-  }
 </style>
 
 <div class="mzp-c-navigation mzp-is-sticky">
@@ -62,16 +26,7 @@
       </div>
       <div class="mzp-c-navigation-menu">
         <div class="mzp-c-navigation-container">
-          <div class="search-autocomplete">
-            <input class="search-input" type="search" />
-            <div class="search-results">
-              <ul>
-                <li>Foo</li>
-                <li>Bar</li>
-                <li>Baz</li>
-              </ul>
-            </div>
-          </div>
+          <SearchBox />
         </div>
       </div>
     </div>
---
+++ src/components/SearchBox.svelte
@@ -0,0 +1,71 @@
+<script>
+  import Pill from "./Pill.svelte";
+</script>
+
+<style>
+  .search-autocomplete {
+    margin-left: 24px;
+    margin-top: 24px;
+    min-width: 600px;
+    position: relative;
+  }
+
+  .search-input {
+    margin: 0;
+    width: 100%;
+  }
+
+  .search-results {
+    position: absolute;
+    border: 1px solid #dbdbdb;
+    background-color: white;
+    width: 100%;
+    z-index: 100;
+  }
+  .search-results ul {
+    margin: 0;
+    padding: 0;
+  }
+
+  .search-results li {
+    padding: 0.25rem 0.5rem;
+    display: grid;
+    grid-template-columns: auto;
+    grid-template-rows: max-content max-content;
+    grid-template-areas:
+      "title type"
+      "description description";
+    cursor: pointer;
+  }
+
+  .search-results li {
+    padding: 0.25rem 0.5rem;
+    cursor: pointer;
+  }
+  .search-results li:hover {
+    background-color: #dbdbdb;
+  }
+
+  .search-results .title {
+    grid-area: title;
+  }
+</style>
+
+<div class="search-autocomplete">
+  <input class="search-input" type="search" />
+  <div class="search-results">
+    <ul>
+      <li>
+        <div class="name">about_page.libraries_tapped</div>
+        <div class="type">
+          <Pill message="metric" bgColor="#4a5568" />
+        </div>
+        <div class="description">
+          A user tapped on "Libraries that we use" item from About page
+        </div>
+      </li>
+      <li>Bar</li>
+      <li>Baz</li>
+    </ul>
+  </div>
+</div>
