979ba0e8a6646b9db4c2107818534041e6daed29
William Lachance <wlachance@mozilla.com>
2021-01-08 16:45:05 -0500

    Improve pill aesthetics (#307)
    * Improve pill aesthetics
    
    Use better font, border choices -- more similar to GitHub labels:
    
    https://primer.style/css/components/labels
    
    * Update snapshot

--- src/components/Pill.svelte
+++ src/components/Pill.svelte
@@ -3,8 +3,18 @@
   export let bgColor;
 </script>
 
-<div
-  style="background-color: {bgColor}"
-  class="inline-block border rounded-full text-xs py-1 px-3 w-15 text-white">
-  {message}
-</div>
+<style>
+  .pill {
+    @apply inline-block;
+    @apply border-transparent;
+    @apply rounded-full;
+    @apply px-2;
+    @apply py-0;
+    @apply text-sm;
+    @apply leading-5;
+    @apply text-white;
+    @apply font-medium;
+  }
+</style>
+
+<div style="background-color: {bgColor}" class="pill">{message}</div>
