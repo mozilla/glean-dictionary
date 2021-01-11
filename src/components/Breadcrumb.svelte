57923a9a8f3817b49823519fd139e1e221957447
Linh Nguyen <linh@hey.com>
2021-01-11 14:51:46 -0800

    Update snapshot test & improve breadcrumb styling

--- src/components/Breadcrumb.svelte
+++ src/components/Breadcrumb.svelte
@@ -5,21 +5,25 @@
 <style>
   ol {
     display: flex;
-  }
-  li {
-    padding-left: $spacing-xs;
-  }
-  a {
-    text-decoration: none;
-    @include text-title-xs;
+    li {
+      padding-left: $spacing-xs;
+      @include text-title-sm;
+      font-weight: bold;
+      a {
+        text-decoration: none;
+      }
+      span {
+        color: $color-dark-gray-20;
+      }
+    }
   }
 </style>
 
 <ol style={!links.length ? 'display: none;' : ''}>
   {#each links as link}
     <li>
-      <a href={link.url}>{link.name}
-      </a>{links.indexOf(link) === links.length - 1 ? '' : '/'}
+      <a href={link.url}>{link.name} </a><span
+        class>{links.indexOf(link) === links.length - 1 ? '' : '/'}</span>
     </li>
   {/each}
 </ol>
