744aceb241848d6a0544d571d60671d55c21230b
William Lachance <wrlach@gmail.com>
2021-01-18 11:10:46 -0500

    Add human-readable bug and issue links in UI (fixes #191)
    Instead of using full URLs or context-free numbers, use
    idiomatic bugzilla and github shortened forms for bugs and
    issues.

---
+++ src/components/BugLink.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { getBugLinkTitle } from "../formatters/links";
+
+  export let ref;
+  const url = ref.startsWith("http")
+    ? ref
+    : `https://bugzilla.mozilla.org/show_bug.cgi?id=${ref}`;
+</script>
+
+<a href={url} target="_blank"> {getBugLinkTitle(url)} </a>
---
+++ tests/formatters.links.test.js
@@ -0,0 +1,35 @@
+import { getBugLinkTitle } from "../src/formatters/links";
+
+describe("Titles for bugzilla URLs", () => {
+  it("works as expected", () => {
+    expect(getBugLinkTitle("https://bugzilla.mozilla.org/1234")).toEqual(
+      "bugzil.la/1234"
+    );
+    expect(getBugLinkTitle("https://bugzilla.mozilla.org/1234#c23")).toEqual(
+      "bugzil.la/1234#c23"
+    );
+    expect(getBugLinkTitle("https://bugzil.la/1234")).toEqual("bugzil.la/1234");
+    expect(getBugLinkTitle("https://bugzil.la/show_bug.cgi?id=1234")).toEqual(
+      "bugzil.la/1234"
+    );
+  });
+});
+
+describe("Titles for github URLs", () => {
+  it("works as expected", () => {
+    expect(
+      getBugLinkTitle("https://github.com/mozilla-mobile/fenix/issues/1234")
+    ).toEqual("mozilla-mobile/fenix#1234");
+    expect(
+      getBugLinkTitle(
+        "https://github.com/mozilla-mobile/fenix/issues/1234#issuecomment-5678"
+      )
+    ).toEqual("mozilla-mobile/fenix#1234-comment");
+  });
+});
+
+describe("Titles for other issue tracker URLs", () => {
+  it("works as expected", () => {
+    expect(getBugLinkTitle("https://jira.com/1234")).toEqual("jira.com/1234");
+  });
+});
