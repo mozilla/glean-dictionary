133ff397a3c55f125c27c13b27eb6e71f32f48c8
William Lachance <wlachance@mozilla.com>
2021-01-21 10:27:08 -0500

    Refine some help text a bit (#337)
    Just some preliminary work before a larger-scale refactoring and
    additions (to follow).

--- src/data/help.js
+++ src/data/help.js
@@ -1,17 +1,21 @@
+const REQUIRED_METRIC_PARAMS_DOCS =
+  "https://mozilla.github.io/glean/book/user/metric-parameters.html#required-metric-parameters";
+const OPTIONAL_METRIC_PARAMS_DOCS =
+  "https://mozilla.github.io/glean/book/user/metric-parameters.html#optional-metric-parameters";
+
 export default {
   bugs: {
     text:
-      "Required. A list of bugs (e.g. Bugzilla or GitHub) that are relevant to this metric. For example, bugs that track its original implementation or later changes to it.",
-    link: "https://mozilla.github.io/glean/book/user/metric-parameters.html",
+      "A list of bugs (e.g. Bugzilla or GitHub) that are relevant to this metric. For example, bugs that track its original implementation or later changes to it.",
+    link: REQUIRED_METRIC_PARAMS_DOCS,
   },
   lifetime: {
     text:
       "Defines the lifetime of the metric. Different lifetimes affect when the metrics value is reset.",
-    link: "https://mozilla.github.io/glean/book/user/metric-parameters.html",
+    link: OPTIONAL_METRIC_PARAMS_DOCS,
   },
-  send_in_pings: {
-    text:
-      "Defines which pings the metric should be sent on. If not specified, the metric is sent on the 'default ping', which is the events ping for events and the metrics ping for everything else. Most metrics don't need to specify this unless they are sent on custom pings.",
-    link: "https://mozilla.github.io/glean/book/user/metric-parameters.html",
+  sendInPings: {
+    text: "Defines which pings the metric is sent in.",
+    link: OPTIONAL_METRIC_PARAMS_DOCS,
   },
 };
