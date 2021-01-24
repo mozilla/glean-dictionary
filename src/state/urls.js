a786d7965f49d6ea821260fd393c2c69ce201333
F. E Noel Nfebe <fenn25.fn@gmail.com>
2021-01-24 20:11:39 +0100

    Use `_` instead of `-` to identify metric names (fixes #313) (#346)
    Signed-off-by: Fenn-CS <fenn25.fn@gmail.com>

--- src/state/urls.js
+++ src/state/urls.js
@@ -2,7 +2,7 @@ export function getItemURL(appName, itemType, itemName) {
   // workaround servers that incorrectly interpret url resources with a "." in
   // them as having an extension (this is common for metrics) -- the glean
   // schema doesn't allow `-`'s in metrics, so this should be ok
-  return `/apps/${appName}/${itemType}/${itemName.replace(/\./g, "-")}`;
+  return `/apps/${appName}/${itemType}/${itemName.replace(/\./g, "_")}`;
 }
 
 export function getMetricBigQueryURL(appName, pingName, metricName) {
