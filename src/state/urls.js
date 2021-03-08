15e48bea4dd2f02665e950d5929bbf18bc8f2977
William Lachance <wrlach@gmail.com>
2021-03-08 14:06:59 -0500

    More fixes and hacks

--- src/state/urls.js
+++ src/state/urls.js
@@ -2,7 +2,7 @@ function getResourceName(name) {
   // workaround servers that incorrectly interpret url resources with a "." in
   // them as having an extension (this is common for metrics and application ids) -- the glean
   // schema doesn't allow `-`'s in metrics, so this should be ok
-  return name.replace(/\./g, "_").lower();
+  return name.replace(/\./g, "_").toLowerCase();
 }
 
 export function getItemURL(appName, itemType, itemName) {
