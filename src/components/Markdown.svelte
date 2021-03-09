2215635551f17fbcb96971cddac793d3c080d833
William Lachance <wlachance@mozilla.com>
2021-03-09 10:18:18 -0500

    Markdown improvements (#455)
    * Add option to not display inline, which is what we want for larger
      free-form text
    * Fix display of lists by using protocol's styles

--- src/components/Markdown.svelte
+++ src/components/Markdown.svelte
@@ -1,9 +1,29 @@
 <script>
-  import { parseInline } from "marked";
+  // you would think we could just do:
+  // import marked from "marked", but that seems to cause
+  // weird problems with jest and storyshots (probably because
+  // of how it imports/compiles ES modules). this is the only
+  // workaround I found
+  import { use, parse, parseInline } from "marked";
+
+  const renderer = {
+    list(body, ordered, start) {
+      const outerEl = ordered ? "ol" : "ul";
+      return `<${outerEl} ${
+        start ? `start="${start}"` : ""
+      } class="mzp-u-list-styled">${body}</${outerEl}>`;
+    },
+  };
+  use({ renderer });
 
   export let text;
+
+  // if inline is set, do not wrap the markdown in a paragraph -- useful for short snippits
+  export let inline = true;
 </script>
 
-<slot>
+{#if inline}
   {@html parseInline(text)}
-</slot>
+{:else}
+  {@html parse(text)}
+{/if}
