4627f73435ae33a73df55d5f897c4d750492d7c3
John Rafael <dragonwood0@protonmail.com>
2021-03-17 03:00:56 +0800

    Refactored keys to use snake_casing (#459)

--- tests/formatters.searchfox.test.js
+++ tests/formatters.searchfox.test.js
@@ -2,11 +2,11 @@ import { getSearchfoxLink } from "../src/formatters/searchfox";
 
 describe("Searchfox links for firefox-desktop", () => {
   it("works as expected", () => {
-    expect(getSearchfoxLink("firefox-desktop", "test.metric_name")).toEqual(
+    expect(getSearchfoxLink("firefox_desktop", "test.metric_name")).toEqual(
       "https://searchfox.org/mozilla-central/search?q=test.metricName|Test.metricName|test.metric_name|test::metric_name&regexp=true"
     );
     expect(
-      getSearchfoxLink("firefox-desktop", "test_category.test_name")
+      getSearchfoxLink("firefox_desktop", "test_category.test_name")
     ).toEqual(
       "https://searchfox.org/mozilla-central/search?q=testCategory.testName|TestCategory.testName|test_category.test_name|test_category::test_name&regexp=true"
     );
