17f1c2003049aee5c62b922b1e513cac7029db76
William Lachance <wrlach@gmail.com>
2021-03-26 17:47:49 -0400

    Fix tests

--- tests/formatters.codesearch.test.js
+++ tests/formatters.codesearch.test.js
@@ -3,12 +3,12 @@ import { getCodeSearchLink } from "../src/formatters/codesearch";
 describe("Searchfox links for firefox-desktop", () => {
   it("works as expected", () => {
     expect(getCodeSearchLink("firefox_desktop", "test.metric_name")).toEqual(
-      "https://searchfox.org/mozilla-central/search?q=test.metricName|Test.metricName|test.metric_name|test::metric_name&regexp=true"
+      "https://searchfox.org/mozilla-central/search?q=test.metric_name|test.metricName|Test.metricName|test.metric_name|test::metric_name&regexp=true"
     );
     expect(
       getCodeSearchLink("firefox_desktop", "test_category.test_name")
     ).toEqual(
-      "https://searchfox.org/mozilla-central/search?q=testCategory.testName|TestCategory.testName|test_category.test_name|test_category::test_name&regexp=true"
+      "https://searchfox.org/mozilla-central/search?q=test_category.test_name|testCategory.testName|TestCategory.testName|test_category.test_name|test_category::test_name&regexp=true"
     );
   });
 });
@@ -16,10 +16,10 @@ describe("Searchfox links for firefox-desktop", () => {
 describe("Searchfox links for applications in mozilla-mobile", () => {
   it("works as expected", () => {
     expect(getCodeSearchLink("fenix", "test.metric_name")).toEqual(
-      "https://searchfox.org/mozilla-mobile/search?q=test.metricName|Test.metricName|test.metric_name|test::metric_name&path=fenix&regexp=true"
+      "https://searchfox.org/mozilla-mobile/search?q=test.metric_name|test.metricName|Test.metricName|test.metric_name|test::metric_name&path=fenix&regexp=true"
     );
     expect(getCodeSearchLink("fenix", "test_category.test_name")).toEqual(
-      "https://searchfox.org/mozilla-mobile/search?q=testCategory.testName|TestCategory.testName|test_category.test_name|test_category::test_name&path=fenix&regexp=true"
+      "https://searchfox.org/mozilla-mobile/search?q=test_category.test_name|testCategory.testName|TestCategory.testName|test_category.test_name|test_category::test_name&path=fenix&regexp=true"
     );
   });
 });
@@ -27,7 +27,7 @@ describe("Searchfox links for applications in mozilla-mobile", () => {
 describe("returns Sourcegraph link if the application is not indexed on Searchfox", () => {
   it("works as expected", () => {
     expect(getCodeSearchLink("mozregression", "usage.bad_date")).toEqual(
-      "https://sourcegraph.com/search?q=repo:%5Egithub%5C.com%5C/%5BMm%5Dozilla%28.*%29%5C/mozregression%24+usage.badDate|Usage.badDate|usage.bad_date|usage::bad_date&patternType=regexp"
+      "https://sourcegraph.com/search?q=repo:%5Egithub%5C.com%5C/%5BMm%5Dozilla%28.*%29%5C/mozregression%24+usage.bad_date|usage.badDate|Usage.badDate|usage.bad_date|usage::bad_date&patternType=regexp"
     );
   });
 });
