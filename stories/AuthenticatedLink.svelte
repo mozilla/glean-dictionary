234686ea9e67b419e960724e07cb43f0247377bb
William Lachance <wrlach@gmail.com>
2021-05-21 11:48:09 -0400

    Appease eslint + storybook tests

--- stories/AuthenticatedLink.svelte
+++ stories/AuthenticatedLink.svelte
@@ -1,7 +1,8 @@
 <script>
   import AuthenticatedLink from "../src/components/AuthenticatedLink.svelte";
 
-  export let title, href;
+  export let href;
+  export let title;
 </script>
 
 <AuthenticatedLink {href}>{title}</AuthenticatedLink>
