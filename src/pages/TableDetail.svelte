837d27d74a952849092d37cb834b63a404529075
William Lachance <wrlach@gmail.com>
2021-08-24 11:09:49 -0400

    Improve documentation and help in table view page (fixes #787)

--- src/pages/TableDetail.svelte
+++ src/pages/TableDetail.svelte
@@ -1,4 +1,5 @@
 <script>
+  import HelpHoverable from "../components/HelpHoverable.svelte";
   import SchemaViewer from "../components/SchemaViewer.svelte";
   import { getTableData } from "../state/api";
   import { pageTitle } from "../state/stores";
@@ -19,22 +20,56 @@
     <col />
     <col />
     <tr>
-      <td>BigQuery Definition</td>
+      <td>
+        Stable
+        <HelpHoverable
+          content={"BigQuery table containing stable ping data, rolled up once per day with additional deduplication applied as well as partitioning by sample id. Use this table for most analysis."}
+          link={"https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html#table-layout-and-naming"}
+        />
+      </td>
+      <td>
+        <code>{table.stable_table}</code>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Live
+        <HelpHoverable
+          content={"BigQuery table containing live ping data as it is submitted to our pipeline. Only use this table if you need results for the current (partial) day."}
+          link={"https://docs.telemetry.mozilla.org/cookbooks/bigquery/querying.html#table-layout-and-naming"}
+        />
+      </td>
+      <td>
+        <code>{table.live_table}</code>
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Schema
+        <HelpHoverable
+          content={"BigQuery schema definition for this table."}
+          link={"https://docs.telemetry.mozilla.org/concepts/pipeline/schemas.html#mozilla-schema-generator"}
+        />
+      </td>
       <td>
         <a href={table.bq_definition}>
           {table.bq_definition.split("/").slice(-1)}
         </a>
       </td>
     </tr>
-    <tr>
-      <td>Live Data</td>
-      <td><code>{table.live_table}</code></td>
-    </tr>
-    <tr>
-      <td>Stable View</td>
-      <td><code>{table.stable_table}</code></td>
-    </tr>
   </table>
+  <p>
+    To access BigQuery data directly, you need to write queries using SQL. For
+    more information on how to do this, see
+    <a
+      href="https://docs.telemetry.mozilla.org/cookbooks/accessing_glean_data.html"
+    >
+      Accessing Glean Data
+    </a>
+    on
+    <a href="https://docs.telemetry.mozilla.org">docs.telemetry.mozilla.org</a>
+    .
+  </p>
   <SchemaViewer app={params.app} nodes={table.bq_schema} />
 {:catch}
   <NotFound pageName={params.appId} itemType="table" />
