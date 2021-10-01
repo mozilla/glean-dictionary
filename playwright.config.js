06623a14d63e6009e7b1a7b43f9761b6b2efe4ca
Linh Nguyen <linhnguyen@mozilla.com>
2021-10-01 15:03:08 -0400

    Use a dev webserver for testing

--- playwright.config.js
+++ playwright.config.js
@@ -1,4 +1,10 @@
 const config = {
+  webServer: {
+    command: "npm run start",
+    port: 5000,
+    timeout: 120 * 1000,
+    reuseExistingServer: !process.env.CI,
+  },
   testDir: "playwright",
 };
 
--- playwright/test.spec.js
+++ playwright/test.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
 test("first playwright test", async ({ page }) => {
-  await page.goto("https://dictionary.telemetry.mozilla.org");
+  await page.goto("http://localhost:5000");
   await expect(page).toHaveTitle(/Glean Dictionary/);
 });
