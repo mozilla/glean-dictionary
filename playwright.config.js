92fd181ad100a892ff44d8614007e63aa86a4a37
Will Lachance <wlachance@mozilla.com>
2021-11-10 12:47:30 -0500

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -1,5 +1,6 @@
 const config = {
   webServer: {
+    // run sirv in single-page application mode so non-root URLs (e.g. /apps/fenix/pings/metrics) work
     command: "npm run start -- --single",
     port: 5000,
     timeout: 120 * 1000,
