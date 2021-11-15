9f7e9a34e658cb40d087f8cc97936f84860870d3
Will Lachance <wlachance@mozilla.com>
2021-11-15 08:49:29 -0500

    Add a playwright end-to-end test (#950)
    Just starting with the metrics ping for now (motivated by #949).

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,7 @@
 const config = {
   webServer: {
-    command: "npm run start",
+    // run sirv in single-page application mode so non-root URLs (e.g. /apps/fenix/pings/metrics) work
+    command: "npm run start -- --single",
     port: 5000,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
--- playwright/test.spec.js
+++ playwright/test.spec.js
@@ -4,3 +4,14 @@ test("first playwright test", async ({ page }) => {
   await page.goto("http://localhost:5000");
   await expect(page).toHaveTitle(/Glean Dictionary/);
 });
+
+test("metrics ping page", async ({ page }) => {
+  await page.goto("http://localhost:5000/apps/fenix/pings/metrics");
+  await expect(page).toHaveTitle("metrics | fenix");
+
+  // we expect a selector starting with our looker URL, directing to an explore
+  // containing the word "metrics" corresponding to the ping
+  await expect(
+    page.locator('a[href^="https://mozilla.cloud.looker"]')
+  ).toContainText("metrics");
+});
