3ec40f1b03c521cef9f2c2bd1e7aa5e6e3a81130
Linh Nguyen <linhnguyen@mozilla.com>
2022-02-02 16:05:33 -0500

    Use toBe instead of toEqual in jest

--- tests/formatters.codesearch.test.js
+++ tests/formatters.codesearch.test.js
@@ -2,12 +2,12 @@ import { getCodeSearchLink } from "../src/formatters/codesearch";
 
 describe("Searchfox links for firefox-desktop", () => {
   it("works as expected", () => {
-    expect(getCodeSearchLink("firefox_desktop", "test.metric_name")).toEqual(
+    expect(getCodeSearchLink("firefox_desktop", "test.metric_name")).toBe(
       "https://searchfox.org/mozilla-central/search?q=test.metric_name|test.metricName|Test.metricName|test.metric_name|test::metric_name&regexp=true"
     );
     expect(
       getCodeSearchLink("firefox_desktop", "test_category.test_name")
-    ).toEqual(
+    ).toBe(
       "https://searchfox.org/mozilla-central/search?q=test_category.test_name|testCategory.testName|TestCategory.testName|test_category.test_name|test_category::test_name&regexp=true"
     );
   });
@@ -15,10 +15,10 @@ describe("Searchfox links for firefox-desktop", () => {
 
 describe("Searchfox links for applications in mozilla-mobile", () => {
   it("works as expected", () => {
-    expect(getCodeSearchLink("fenix", "test.metric_name")).toEqual(
+    expect(getCodeSearchLink("fenix", "test.metric_name")).toBe(
       "https://searchfox.org/mozilla-mobile/search?q=test.metric_name|test.metricName|Test.metricName|test.metric_name|test::metric_name&path=fenix&regexp=true"
     );
-    expect(getCodeSearchLink("fenix", "test_category.test_name")).toEqual(
+    expect(getCodeSearchLink("fenix", "test_category.test_name")).toBe(
       "https://searchfox.org/mozilla-mobile/search?q=test_category.test_name|testCategory.testName|TestCategory.testName|test_category.test_name|test_category::test_name&path=fenix&regexp=true"
     );
   });
@@ -26,7 +26,7 @@ describe("Searchfox links for applications in mozilla-mobile", () => {
 
 describe("returns Sourcegraph link if the application is not indexed on Searchfox", () => {
   it("works as expected", () => {
-    expect(getCodeSearchLink("mozregression", "usage.bad_date")).toEqual(
+    expect(getCodeSearchLink("mozregression", "usage.bad_date")).toBe(
       "https://sourcegraph.com/search?q=repo:%5Egithub%5C.com%5C/%5BMm%5Dozilla%28.*%29%5C/mozregression%24+usage.bad_date|usage.badDate|Usage.badDate|usage.bad_date|usage::bad_date&patternType=regexp"
     );
   });
