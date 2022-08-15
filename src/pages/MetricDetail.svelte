0318633412ec2e3f534ca186213882bc1d6bdb39
Alessio Placitelli <alessio.placitelli@gmail.com>
2022-08-15 12:40:58 +0200

    Update the base documentation URL
    This is required since we don't have redirects from the old location to the new one for the most recent metric types.

--- src/pages/MetricDetail.svelte
+++ src/pages/MetricDetail.svelte
@@ -68,7 +68,7 @@
   }
 
   function getMetricDocumentationURI(type) {
-    const sourceDocs = "https://mozilla.github.io/glean/book/user/metrics/";
+    const sourceDocs = "https://mozilla.github.io/glean/book/reference/metrics/";
     const links = {
       memory_distribution: "memory_distribution.html",
       quantity: "quantity.html",
