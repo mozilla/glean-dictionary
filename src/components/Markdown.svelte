0802a3fba35278bbc0d159bbd00670d38b10c23b
Jan-Erik Rediger <jrediger@mozilla.com>
2024-02-26 12:06:34 +0100

    Add a header anchor link to every header rendered from commentary (#2079)
    * Add a header anchor link to every header rendered from commentary
    
    Fixes https://github.com/mozilla/glean-annotations/issues/217
    
    * Add header styling to a markdown block

--- src/components/Markdown.svelte
+++ src/components/Markdown.svelte
@@ -13,6 +13,15 @@
         start ? `start="${start}"` : ""
       } class="mzp-u-list-styled">${body}</${outerEl}>`;
     },
+    heading(text, level, raw) {
+      const id = raw
+        .toLowerCase()
+        .trim()
+        .replace(/<[!/a-z].*?>/gi, "")
+        .replace(/ +/g, "-");
+
+      return `<h${level} class="annotation-header-link" id="${id}"><a href="#${id}">${text}</a></h${level}>\n`;
+    },
   };
   use({ renderer });
 
@@ -24,5 +33,26 @@
 {#if inline}
   {@html parseInline(text)}
 {:else}
-  {@html parse(text)}
+  <div>
+    {@html parse(text)}
+  </div>
 {/if}
+
+<style lang="scss">
+  div :global(h1 a),
+  div :global(h2 a),
+  div :global(h3 a),
+  div :global(h4 a),
+  div :global(h5 a) {
+    color: black;
+    text-decoration: none;
+  }
+
+  div :global(h1 a:hover),
+  div :global(h2 a:hover),
+  div :global(h3 a:hover),
+  div :global(h4 a:hover),
+  div :global(h5 a:hover) {
+    text-decoration: underline;
+  }
+</style>
