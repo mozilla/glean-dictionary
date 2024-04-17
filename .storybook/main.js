fd46a0322d1732c6718ecfca3e015cc0f205a3d8
Alessio Placitelli <alessio.placitelli@gmail.com>
2024-04-17 12:54:33 +0200

    Update to storybook 8.0.8 (#2195)
    * Update to storybook 8.0.8
    
    * Remove the deprecated CLI switch
    
    * Disable storybook telemetry
    
    * WIP: attempt fixing ci

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,10 @@ const autoPrefixer = require("autoprefixer");
 const path = require("path");
 
 module.exports = {
+  core: {
+    disableTelemetry: true,
+  },
+  staticDirs: ["../public"],
   stories: ["../stories/**/*.stories.js"],
   addons: ["@storybook/addon-controls", "@storybook/addon-docs"],
 
