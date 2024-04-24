2048d94cb78a797e8be255241c6814d96075a1d5
Alessio Placitelli <alessio.placitelli@gmail.com>
2024-04-24 13:07:04 +0200

    Bug 1887694 - Generate events_stream SQL for events (#2196)
    * Generate events_stream SQL for events
    
    * feat: use the old sql query for events in custom pings
    
    * style: remove leading spaces to ease copy/paste

--- src/pages/MetricDetail.svelte
+++ src/pages/MetricDetail.svelte
@@ -58,7 +58,14 @@
     additionalInfo
   ) {
     if (metricType === "event") {
-      return getGleanEventQuery(table, additionalInfo);
+      // Although events might come from other pings, we override that and
+      // generate SQL just for the `events_stream`. This changes
+      // `some_database.table` to `some_database.events_stream`.
+      const tableNameParts = table.split('.');
+      const override = `${tableNameParts[0]}.events_stream`;
+      return tableNameParts[1] === "events"
+        ? getGleanEventQuery(override, additionalInfo)
+        : getGleanLegacyEventQuery(table, additionalInfo);
     }
 
     return getGleanQuery(columnName, table);
