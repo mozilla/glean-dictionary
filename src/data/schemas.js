eb4445ae945ae184c8decb6302ff36e35c6eef9c
Charlie Humphreys <chumphreys@mozilla.com>
2026-07-21 15:57:50 -0400

    #2309: Show object metric structure in the metric detail view

--- src/data/schemas.js
+++ src/data/schemas.js
@@ -9,11 +9,14 @@ import {
 import { getCodeSearchLink } from "../formatters/codesearch";
 import { getPingReasons } from "../formatters/text";
 import { formatMonitor } from "../formatters/monitor";
+import { getStructureMarkdown } from "../formatters/structure";
 
 const REQUIRED_METRIC_PARAMS_DOCS =
   "https://mozilla.github.io/glean/book/user/metric-parameters.html#required-metric-parameters";
 const OPTIONAL_METRIC_PARAMS_DOCS =
   "https://mozilla.github.io/glean/book/user/metric-parameters.html#optional-metric-parameters";
+const EXAMPLE_OBJECT_DEFINITION_DOC =
+  "https://mozilla.github.io/glean/book/reference/metrics/object.html#example-object-metric-definition";
 const PING_REASONS_DOCS =
   "https://mozilla.github.io/glean/book/reference/yaml/pings.html#reasons";
 const METRIC_MONITOR_DOCS =
@@ -232,6 +235,14 @@ export const METRIC_METADATA_SCHEMA = [
       "Information about telemetry alerting monitoring for this metric. Click to read more.",
     helpLink: METRIC_MONITOR_DOCS,
     valueFormatter: formatMonitor,
+  },
+  {
+    title: "Structure",
+    id: "structure",
+    type: "markdown",
+    helpText: "The defined structure of the object metric.",
+    helpLink: EXAMPLE_OBJECT_DEFINITION_DOC,
+    valueFormatter: getStructureMarkdown,
   },
 ];
 export const PING_SCHEMA = [
