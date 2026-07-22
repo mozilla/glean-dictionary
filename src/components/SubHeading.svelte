0520ea3c47c0ea1710731e35d112cd6de46fb653
Charlie Humphreys <chumphreys@mozilla.com>
2026-07-22 09:08:09 -0500

    add header/event extra links to metric detail page (#2335)

--- src/components/SubHeading.svelte
+++ src/components/SubHeading.svelte
@@ -3,16 +3,30 @@
 
   export let title;
   export let helpText;
+  export let withLink;
 </script>
 
-<h2>
-  {@html title}
-  {#if helpText}
-    <HelpHoverable content={helpText} />
-  {/if}
-</h2>
+{#if withLink}
+  <a href="#{title}">
+    <h2 id={title} class="anchor">
+      {@html title}
+      {#if helpText}
+        <HelpHoverable content={helpText} />
+      {/if}
+    </h2>
+  </a>
+{:else}
+  <h2>
+    {@html title}
+    {#if helpText}
+      <HelpHoverable content={helpText} />
+    {/if}
+  </h2>
+{/if}
 
 <style lang="scss">
+  @import "../main.scss";
+
   h2 {
     @include text-title-xs;
   }
